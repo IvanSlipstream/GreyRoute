5e9f2afc426d265eba04b2f7e100158211ef9f12
Slipstream <ivan_slipstream@ukr.net>
2016-12-05 16:21:52 +0200

    added parcelable implementaion

--- app/src/main/java/com/gmsworldwide/kharlamov/greyroute/models/SmsBriefData.java
+++ app/src/main/java/com/gmsworldwide/kharlamov/greyroute/models/SmsBriefData.java
@@ -2,13 +2,15 @@ package com.gmsworldwide.kharlamov.greyroute.models;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import com.gmsworldwide.kharlamov.greyroute.service.SmsIntentService;
 
 /**
  * Created by Slipstream on 30.07.2016.
  */
-public class SmsBriefData {
+public class SmsBriefData implements Parcelable {
 
     private String mSmsc;
     private String mText;
@@ -24,6 +26,36 @@ public class SmsBriefData {
         this.mSmsc = cursor.getString(cursor.getColumnIndex("service_center"));
         this.mText = cursor.getString(cursor.getColumnIndex("body"));
         this.mTpOa = cursor.getString(cursor.getColumnIndex("address"));
+    }
+
+    protected SmsBriefData(Parcel in) {
+        mSmsc = in.readString();
+        mText = in.readString();
+        mTpOa = in.readString();
+    }
+
+    public static final Creator<SmsBriefData> CREATOR = new Creator<SmsBriefData>() {
+        @Override
+        public SmsBriefData createFromParcel(Parcel in) {
+            return new SmsBriefData(in);
+        }
+
+        @Override
+        public SmsBriefData[] newArray(int size) {
+            return new SmsBriefData[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(mSmsc);
+        parcel.writeString(mText);
+        parcel.writeString(mTpOa);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     public String getSmsc() {
