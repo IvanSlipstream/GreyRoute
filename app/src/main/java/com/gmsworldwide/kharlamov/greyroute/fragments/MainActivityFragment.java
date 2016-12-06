337f0ad1c311dd0069cc27922786791332f59d10
Slipstream <ivan_slipstream@ukr.net>
2016-12-06 18:04:11 +0200

    added retain instance mechanism

--- app/src/main/java/com/gmsworldwide/kharlamov/greyroute/fragments/MainActivityFragment.java
+++ app/src/main/java/com/gmsworldwide/kharlamov/greyroute/fragments/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package com.gmsworldwide.kharlamov.greyroute.fragments;
 
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -7,6 +8,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.ResultReceiver;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
@@ -28,12 +30,31 @@ import java.util.ArrayList;
 public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList<SmsBriefData>> {
 
     public static final Uri URI_SMS_INBOX = Uri.parse("content://sms/inbox");
-    private static final int LOADER_ID_INBOX = 1;
     public static final String INBOX_SORT_ORDER = "date";
+
+    private static final int LOADER_ID_INBOX = 1;
+    private static final String RETAIN_INSTANCE_KEY_SMS_LIST = "sms_list";
     private RecyclerView mRecyclerView;
     private SmsListAdapter adapter;
 
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new SmsListAdapter();
+        if (savedInstanceState != null) {
+            adapter.setSmsBriefDataList(savedInstanceState.<SmsBriefData>getParcelableArrayList(RETAIN_INSTANCE_KEY_SMS_LIST));
+        }
+        ResultReceiver mReceiver = new ResultReceiver(new Handler()) {
+            @Override
+            protected void onReceiveResult(int resultCode, Bundle resultData) {
+                adapter.addSmsBriefData(new SmsBriefData(resultData));
+            }
+        };
+        SmsIntentService.startActionSetListener(getActivity(), mReceiver);
+        getActivity().getSupportLoaderManager().initLoader(LOADER_ID_INBOX, null, this);
     }
 
     @Override
@@ -42,17 +63,15 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
         View mView = inflater.inflate(R.layout.fragment_main, container, false);
         mRecyclerView = (RecyclerView) mView.findViewById(R.id.rvSmsList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        adapter = new SmsListAdapter();
-        ResultReceiver mReceiver = new ResultReceiver(new Handler()) {
-            @Override
-            protected void onReceiveResult(int resultCode, Bundle resultData) {
-                adapter.addSmsBriefData(new SmsBriefData(resultData));
-                mRecyclerView.setAdapter(adapter);
-            }
-        };
-        SmsIntentService.startActionSetListener(getActivity(), mReceiver);
-        getActivity().getSupportLoaderManager().initLoader(LOADER_ID_INBOX, null, this);
+        mRecyclerView.setAdapter(adapter);
         return mView;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        SmsListAdapter adapter = (SmsListAdapter) mRecyclerView.getAdapter();
+        outState.putParcelableArrayList(RETAIN_INSTANCE_KEY_SMS_LIST, adapter.getSmsBriefDataList());
     }
 
     @Override
@@ -96,6 +115,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
         private TextView tvSmscOaAddress;
         private TextView tvText;
         private int mUnmarkedColor;
+        private int mMarkedColor;
         private boolean mMarked;
 
         public SmsHolder(View itemView) {
@@ -103,6 +123,8 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
             tvSmscOaAddress = (TextView) itemView.findViewById(android.R.id.text1);
             tvText = (TextView) itemView.findViewById(android.R.id.text2);
             Drawable background = itemView.getBackground();
+//            Resources.Theme theme = itemView.getContext().getTheme();
+//            mMarkedColor = theme.getResources().getColor(android.R.color.background_dark, theme);
             if (background instanceof ColorDrawable){
                 mUnmarkedColor = ((ColorDrawable) background).getColor();
             } else {
@@ -114,6 +136,7 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
             if (marked) {
                 itemView.setBackgroundColor(Color.GREEN);
                 this.mMarked = true;
+                // TODO bind color to the theme and fix hardcoded color
             } else {
                 itemView.setBackgroundColor(mUnmarkedColor);
                 this.mMarked = false;
@@ -140,10 +163,19 @@ public class MainActivityFragment extends Fragment implements LoaderManager.Load
             this.mSmsBriefDataList = new ArrayList<>();
         }
 
+        public ArrayList<SmsBriefData> getSmsBriefDataList() {
+            return mSmsBriefDataList;
+        }
+
+        public void setSmsBriefDataList(ArrayList<SmsBriefData> smsBriefDataList) {
+            this.mSmsBriefDataList = smsBriefDataList;
+        }
+
         @Override
         public SmsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(parent.getContext());
             View view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+            // TODO: make custom layout
             return new SmsHolder(view);
         }
 
