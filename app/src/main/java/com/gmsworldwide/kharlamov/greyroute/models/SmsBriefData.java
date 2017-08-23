f92367b6cb47b6b9b475d85f3245747da17f16b9
Slipstream-DESKTOP <ivan_slipstream@ukr.net>
2017-08-23 10:45:07 +0300

    added timestamp and corrected time formatting

--- app/src/main/java/com/gmsworldwide/kharlamov/greyroute/models/SmsBriefData.java
+++ app/src/main/java/com/gmsworldwide/kharlamov/greyroute/models/SmsBriefData.java
@@ -4,6 +4,10 @@ import android.database.Cursor;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Slipstream on 30.07.2016.
  */
@@ -12,6 +16,7 @@ public class SmsBriefData implements Parcelable {
     private String mSmsc;
     private String mText;
     private String mTpOa;
+    private long mTime;
 
     public SmsBriefData(String smsc, String text, String tpOa) {
         this.mSmsc = smsc;
@@ -19,16 +24,25 @@ public class SmsBriefData implements Parcelable {
         this.mTpOa = tpOa;
     }
 
+    public SmsBriefData(String smsc, String text, String tpOa, long time) {
+        this.mSmsc = smsc;
+        this.mText = text;
+        this.mTpOa = tpOa;
+        this.mTime = time;
+    }
+
     public SmsBriefData (Cursor cursor) {
         this.mSmsc = cursor.getString(cursor.getColumnIndex("service_center"));
         this.mText = cursor.getString(cursor.getColumnIndex("body"));
         this.mTpOa = cursor.getString(cursor.getColumnIndex("address"));
+        this.mTime = cursor.getLong(cursor.getColumnIndex("date"));
     }
 
     private SmsBriefData(Parcel in) {
         mSmsc = in.readString();
         mText = in.readString();
         mTpOa = in.readString();
+        mTime = in.readLong();
     }
 
     public static final Creator<SmsBriefData> CREATOR = new Creator<SmsBriefData>() {
@@ -48,6 +62,7 @@ public class SmsBriefData implements Parcelable {
         parcel.writeString(mSmsc);
         parcel.writeString(mText);
         parcel.writeString(mTpOa);
+        parcel.writeLong(mTime);
     }
 
     @Override
@@ -65,5 +80,10 @@ public class SmsBriefData implements Parcelable {
 
     public String getTpOa() {
         return mTpOa;
+    }
+
+    public String getFormattedTime(){
+        Date date = new Date(mTime);
+        return DateFormat.getDateTimeInstance().format(date);
     }
 }
